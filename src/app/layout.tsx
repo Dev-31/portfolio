08a1ba4f3f7441a9e4f041bc31cfa3470517e0f4
Dev-31 <141649798+Dev-31@users.noreply.github.com>
2025-12-08 20:56:22 +0530

    feat: mind-blowing loader animation like Lando Norris

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 <script defer src="https://cloud.umami.is/share/GKwBpucVrJ4sEUVR" data-website-id="4d810ae3-2cfe-421e-a228-a3d23e40292c"></script>
 import type { Metadata } from 'next'
+import { useState } from 'react';
+import SiteLoader from '../components/Loader/SiteLoader';
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -15,16 +17,33 @@ export const metadata: Metadata = {
   },
 }
 
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <html lang="en" className="scroll-smooth">
+//       <body className="bg-[#0a0a0a] text-gray-100 antialiased">
+//         {children}
+//       </body>
+//     </html>
+//   )
+// }
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const [loadingComplete, setLoadingComplete] = useState(false);
+
   return (
     <html lang="en" className="scroll-smooth">
-      <body className="bg-[#0a0a0a] text-gray-100 antialiased">
-        {children}
+      <body className="bg-black text-gray-100 antialiased">
+        <SiteLoader onLoadComplete={() => setLoadingComplete(true)} />
+        {loadingComplete && children}
       </body>
     </html>
-  )
+  );
 }
